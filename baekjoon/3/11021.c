06246b04580c4a17fbefc06952b7591c957bc541
tjwjdgus <wjdgus359911@gmail.com>
2020-08-01 15:31:53 +0900

    0

---
+++ baekjoon/3/11021.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void) {
+	int num;
+	scanf("%d", &num);
+	int n1, n2;
+	for (int i=1; i<=num; i++) {
+		scanf("%d%d", &n1, &n2);
+		printf("Case #%d: %d\n",i, n1 + n2);
+	}
+	return 0;
+}
