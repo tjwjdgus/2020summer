2f6cc690b01886740214b3102b7f1e5724d2f23f
tjwjdgus <wjdgus359911@gmail.com>
2020-08-01 15:13:22 +0900

    0

---
+++ baekjoon/2/9498.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	if (n >= 90)
+		printf("A\n");
+	else if (n >= 80 && n < 90)
+		printf("B\n");
+	else if (n >= 70 && n < 80)
+		printf("C\n");
+	else if (n >= 60 && n < 70)
+		printf("D\n");
+	else
+		printf("F\n");
+	return 0;
+}
