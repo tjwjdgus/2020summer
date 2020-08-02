b09d2fde07f8bdf5be0399a48cc456614e56d576
tjwjdgus <wjdgus359911@gmail.com>
2020-08-02 13:50:04 +0900

    0

---
+++ baekjoon/6/8958.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main(void) {
+	int num;
+	scanf("%d", &num);
+	char str[81];
+	int c = 0, l = 0;
+	for (int i = 0; i < num; i++) {
+		scanf("%s", str);
+		int sum = 0, count = 0;
+		for (int j = 0; j < strlen(str); j++) {
+			if (str[j] == 'o' || str[j] == 'O') {
+				count++;
+				sum += count;
+			}
+			else
+				count = 0;
+		}
+		printf("%d\n", sum);
+	}
+	return 0;
+}
